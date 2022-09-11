28d1f39d7d9e4ce06615389588f978c447735ff7
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-11 19:18:18 +0300

    finished 4th query in unidb

--- uni.sql
+++ uni.sql
@@ -6,3 +6,6 @@ SELECT `name` FROM `course` WHERE start_time = (SELECT min(start_time) FROM cour
 
 -- Find all courses taken by BIF majors
 SELECT c.name FROM `course` as c, majorsln as m , enrolled as e , department as d WHERE e.student_id = m.student_id and d.id = m.dept_id and e.Course_crn = c.crn and d.name = 'BIF'  GROUP by c.name
+
+-- Create a list of all students who are not enrolled in a course
+SELECT id from student where id not in (SELECT student_id FROM enrolled)
