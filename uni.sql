e172717cb2bb696d630dbeff6ec46073b4075640
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-11 18:03:41 +0300

    solved 2nd query in unidb

--- uni.sql
+++ uni.sql
@@ -1,2 +1,5 @@
 -- Find all rooms that can seat at least 100 people
 SELECT * FROM `room` WHERE capacity >= 100 
+
+-- Find the course or courses with the earliest start time
+SELECT `crn`, `start_time` FROM `course` where start_time = (SELECT MIN(start_time) FROM `course`)
