394346e4fa2687325bda65f306b2a23cab8b146a
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-11 19:16:12 +0300

    finished 3rd query in unidb

--- uni.sql
+++ uni.sql
@@ -3,3 +3,6 @@ SELECT * FROM `room` WHERE capacity >= 100
 
 -- Find the course or courses with the earliest start time
 SELECT `name` FROM `course` WHERE start_time = (SELECT min(start_time) FROM course)  
+
+-- Find all courses taken by BIF majors
+SELECT c.name FROM `course` as c, majorsln as m , enrolled as e , department as d WHERE e.student_id = m.student_id and d.id = m.dept_id and e.Course_crn = c.crn and d.name = 'BIF'  GROUP by c.name
